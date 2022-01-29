b6429bf542c50afce63e17adbb39ccd19fc750e0
KangPvP <82943439+KangPvP@users.noreply.github.com>
2022-01-29 12:52:55 +0100

    initial commit 1.3

--- src/main/java/fr/kangpvp/lastarria/Main.java
+++ src/main/java/fr/kangpvp/lastarria/Main.java
@@ -1,6 +1,10 @@
 package fr.kangpvp.lastarria;
 
+import fr.kangpvp.lastarria.commands.CommandTest;
+import fr.kangpvp.lastarria.listeners.ListenersManager;
 import fr.kangpvp.lastarria.utils.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -9,6 +13,9 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         System.out.println("Salut la famille good ou pas ?");
         System.out.println("Test BR5");
+
+        new ListenersManager(this).RegisterListeners();
+        Bukkit.getPluginCommand("test").setExecutor((CommandExecutor) new CommandTest());
 
 
         //loadConfigManager;
---
+++ src/main/java/fr/kangpvp/lastarria/listeners/players/PlayerJoinListener.java
@@ -0,0 +1,6 @@
+package fr.kangpvp.lastarria.listeners.players;
+
+import org.bukkit.event.Listener;
+
+public class PlayerJoinListener implements Listener {
+}
