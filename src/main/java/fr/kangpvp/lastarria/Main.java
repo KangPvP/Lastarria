93c5c2f8c2f9414dd78cb5e7716fdecc1eec2f2f
KangPvP <82943439+KangPvP@users.noreply.github.com>
2022-01-29 12:03:12 +0100

    initial commit 1.1

--- src/main/java/fr/kangpvp/lastarria/Main.java
+++ src/main/java/fr/kangpvp/lastarria/Main.java
@@ -7,7 +7,7 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-    System.out.println("Salut la famille good ");
+    System.out.println("Salut la famil le good ");
 
 
         //loadConfigManager;
