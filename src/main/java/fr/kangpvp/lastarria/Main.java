f2792b168f75f55ea0cc2ee0860d7625161947de
DraKyxx <les.xkillers@gmail.com>
2022-01-29 12:00:51 +0100

    Modif

--- src/main/java/fr/kangpvp/lastarria/Main.java
+++ src/main/java/fr/kangpvp/lastarria/Main.java
@@ -7,7 +7,7 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-    System.out.println("Salut la pute good ");
+    System.out.println("Salut la street good ou pas ?");
 
 
         //loadConfigManager;
