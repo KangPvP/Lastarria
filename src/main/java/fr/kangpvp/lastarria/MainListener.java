5bc2a3a7cfd1f1653d366ee153d65a816a72bd2e
KangPvP <82943439+KangPvP@users.noreply.github.com>
2022-01-30 13:53:11 +0100

    initial commit 1.8

--- src/main/java/fr/kangpvp/lastarria/MainListener.java
+++ src/main/java/fr/kangpvp/lastarria/MainListener.java
@@ -1,9 +1,11 @@
 package fr.kangpvp.lastarria;
 
+import fr.kangpvp.lastarria.listeners.BlockBreakListener;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -14,5 +16,11 @@ public class MainListener implements Listener {
         Player player = event.getPlayer();
         Location join = new Location(Bukkit.getWorld("viking"), 119.700, 68, 202.252, 89.8f, 5.6f);
         player.teleport(join);
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
+
     }
 }
